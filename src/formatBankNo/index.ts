e7f5f14856d6d0ae167816b0e4310d15470073ac
砰砰枪 <18860976892@163.com>
2024-05-17 16:08:09 +0800

    test(formatbankno): 测试提交校验

--- src/formatBankNo/index.ts
+++ src/formatBankNo/index.ts
@@ -17,6 +17,8 @@ function formatBankNo(bankNo: string): string {
   const testRegexp = /^\d+$/g;
   let str = '';
 
+  let unused = '';
+
   if (!testRegexp.test(bankNo)) {
     throw new SyntaxError('请输入正确的银行卡号');
   } else {
